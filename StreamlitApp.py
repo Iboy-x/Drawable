711375c2be90bc3b84c2b20d58b7a721ec1d0067
Abdul Moiz Adeel <139385400+Iboy-x@users.noreply.github.com>
2025-05-04 15:41:14 +0500

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,6 +1,6 @@
 import cv2
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)  # Try changing this to 1, 2, or other values
 
 if not camera.isOpened():
     print("Error: Webcam is not accessible.")
